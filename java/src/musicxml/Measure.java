bb0149da02299b53a6d0efa5c96cf4f28bcbbe9e
Mario Lang <mlang@delysid.org>
2008-10-22 14:46:07 +0000

    Initial checkin of Measure class

---
+++ java/src/musicxml/Measure.java
@@ -0,0 +1,12 @@
+/* -*- c-basic-offset: 2; -*- */
+package musicxml;
+
+import org.w3c.dom.Element;
+
+public class Measure {
+  private Element measure;
+
+  public Measure(Element measure) {
+    this.measure = measure;
+  }
+}
