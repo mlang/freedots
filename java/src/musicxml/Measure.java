27aaee3110c33d1ffbeda234d58e7548da07a3d8
Mario Lang <mlang@delysid.org>
2008-10-24 13:09:18 +0000

    Added Measure class

--- java/src/musicxml/Measure.java
+++ java/src/musicxml/Measure.java
@@ -4,9 +4,15 @@ package musicxml;
 import org.w3c.dom.Element;
 
 public class Measure {
+  private Part parentPart;
   private Element measure;
 
-  public Measure(Element measure) {
+  public Measure(Element measure, Part part) {
     this.measure = measure;
+    this.parentPart = part;
+  }
+
+  public String getNumber() {
+    return measure.getAttribute("number");
   }
 }
