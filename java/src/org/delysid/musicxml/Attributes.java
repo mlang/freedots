626bb6ad78d6e4e23db4923bf17a22de86b98d5e
Mario Lang <mlang@delysid.org>
2009-01-01 21:23:09 +0000

    add musicxml.Attributes.getDivisions().

--- java/src/org/delysid/musicxml/Attributes.java
+++ java/src/org/delysid/musicxml/Attributes.java
@@ -2,7 +2,18 @@
 package org.delysid.musicxml;
 
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class Attributes extends Musicdata {
   public Attributes(Element element) { super(element); }
+  public int getDivisions() {
+    NodeList nodeList = element.getElementsByTagName("divisions");
+    if (nodeList.getLength() == 1) {
+      Node textNode = nodeList.item(0).getChildNodes().item(0);
+      int divisions = Integer.parseInt(textNode.getNodeValue());
+      return divisions;
+    }
+    return 0;
+  }
 }
