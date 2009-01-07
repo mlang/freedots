8e534a1fe7facf8a90926154117111b3e979682a
Mario Lang <mlang@delysid.org>
2009-01-07 00:44:38 +0000

    Remove now dead code.

---
+++ java/src/org/delysid/music/KeySignature.java
@@ -0,0 +1,30 @@
+/* -*- c-basic-offset: 2; -*- */
+package org.delysid.music;
+
+public class KeySignature {
+  private final int type; // number of sharps (+) or flats (-)
+  private final int[] modifiers;
+	
+  public KeySignature(int type)	{
+    this.type = type;
+		
+    modifiers = new int[7];
+    for (int i = 0; i < 7; i++) modifiers[i] = 0;
+    if (type > 0) {
+      for (int i=0; i<type; i++) modifiers[sharps[i]] = 1;
+    } else if (type < 0) {
+      for (int i=0; i<-type; i++) modifiers[flats[i]] = -1;
+    }
+  }
+	
+  public int getType() { return type; }
+	
+  public int getModifierCount() { return Math.abs(type); }
+	
+  public int getModifier(int reducedRank) {
+    return modifiers[reducedRank];
+  }
+	
+  private static final int[] sharps = {3, 0, 4, 1, 5, 2, 6};
+  private static final int[] flats  = {6, 2, 5, 1, 4, 0, 3};
+}
--- java/src/org/delysid/musicxml/Attributes.java
+++ java/src/org/delysid/musicxml/Attributes.java
@@ -1,6 +1,7 @@
 /* -*- c-basic-offset: 2; -*- */
 package org.delysid.musicxml;
 
+import org.delysid.music.KeySignature;
 import org.delysid.music.TimeSignature;
 
 import org.w3c.dom.Element;
@@ -41,6 +42,15 @@ public class Attributes {
     }
     return null;
   }
+  public Key getKey() {
+    NodeList nodeList = element.getElementsByTagName("key");
+    if (nodeList.getLength() == 1) {
+      return new Key((Element)nodeList.item(0));
+    } else if (nodeList.getLength() > 1) {
+      System.err.println("Unhandled multiple key signatures in <attributes>");
+    }
+    return null;
+  }
   public class Time extends TimeSignature {
     Element element = null;
     public Time(Element element) {
@@ -48,24 +58,41 @@ public class Attributes {
       this.element = element;
     }
   }
-    static int getBeatsFromElement(Element element) throws MusicXMLParseException {
-      NodeList nodeList = element.getElementsByTagName("beats");
-      int nodeCount = nodeList.getLength();
-      if (nodeCount >= 1) {
-	Node textNode = nodeList.item(nodeCount-1).getChildNodes().item(0);
-	
-	return Integer.parseInt(textNode.getNodeValue());
-      }
-      throw new MusicXMLParseException("missing <beats> element");
+  public class Key extends KeySignature {
+    Element element = null;
+    public Key(Element element) {
+      super(getFifthsFromElement(element));
+      this.element = element;
     }
-    static int getBeatTypeFromElement(Element element) throws MusicXMLParseException {
-      NodeList nodeList = element.getElementsByTagName("beat-type");
-      int nodeCount = nodeList.getLength();
-      if (nodeCount >= 1) {
-	Node textNode = nodeList.item(nodeCount-1).getChildNodes().item(0);
-
-	return Integer.parseInt(textNode.getNodeValue());
-      }
-      throw new MusicXMLParseException("missing <beat-type> element");
+  }
+  static int getBeatsFromElement(Element element) throws MusicXMLParseException {
+    NodeList nodeList = element.getElementsByTagName("beats");
+    int nodeCount = nodeList.getLength();
+    if (nodeCount >= 1) {
+      Node textNode = nodeList.item(nodeCount-1).getChildNodes().item(0);
+      
+      return Integer.parseInt(textNode.getNodeValue());
     }
+    throw new MusicXMLParseException("missing <beats> element");
+  }
+  static int getBeatTypeFromElement(Element element) throws MusicXMLParseException {
+    NodeList nodeList = element.getElementsByTagName("beat-type");
+    int nodeCount = nodeList.getLength();
+    if (nodeCount >= 1) {
+      Node textNode = nodeList.item(nodeCount-1).getChildNodes().item(0);
+     
+      return Integer.parseInt(textNode.getNodeValue());
+    }
+    throw new MusicXMLParseException("missing <beat-type> element");
+  }
+  static int getFifthsFromElement(Element element) throws MusicXMLParseException {
+    NodeList nodeList = element.getElementsByTagName("fifths");
+    int nodeCount = nodeList.getLength();
+    if (nodeCount >= 1) {
+      Node textNode = nodeList.item(nodeCount-1).getChildNodes().item(0);
+     
+      return Integer.parseInt(textNode.getNodeValue());
+    }
+    throw new MusicXMLParseException("missing <fifths> element");
+  }
 }
