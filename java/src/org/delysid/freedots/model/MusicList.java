e21a3e65cc47d86c2ca6d4a07152f9ab08b58cc8
Mario Lang <mlang@delysid.org>
2009-01-09 18:46:16 +0000

    Rationale for MusicList class.

--- java/src/org/delysid/freedots/model/MusicList.java
+++ java/src/org/delysid/freedots/model/MusicList.java
@@ -1,6 +1,10 @@
 /* -*- c-basic-offset: 2; -*- */
 package org.delysid.freedots.model;
 
+/* It is never the case that musical objects in the future will affect
+ * objects in the past.  This property can be exploited by sorting all
+ * the objects by their temporal order into a one-dimensional list.
+ */
 public class MusicList extends java.util.ArrayList<Event> {
   public MusicList() {
     super();
