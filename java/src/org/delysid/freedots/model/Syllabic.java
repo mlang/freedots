476d396586bddacfe47233ecc3dddaa818e4f85e
Mario Lang <mlang@delysid.org>
2009-03-12 10:31:21 +0000

    Add copyright and license information to the source code files.

--- java/src/org/delysid/freedots/model/Syllabic.java
+++ java/src/org/delysid/freedots/model/Syllabic.java
@@ -1,5 +1,30 @@
+/* -*- c-basic-offset: 2; -*- */
+/*
+ * FreeDots -- MusicXML to braille music transcription
+ *
+ * Copyright 2008-2009 Mario Lang  All Rights Reserved.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details (a copy is included in the LICENSE.txt file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This software is maintained by Mario Lang <mlang@delysid.org>.
+ */
 package org.delysid.freedots.model;
 
 public enum Syllabic {
-  SINGLE, BEGIN, END, MIDDLE;
+  SINGLE /* single-syllable word */,
+  BEGIN  /* word-beginning syllable */,
+  END    /* word-ending syllable */,
+  MIDDLE /* mid-word syllable */;
 }
--- java/src/org/delysid/freedots/transcription/ValueInterpreter.java
+++ java/src/org/delysid/freedots/transcription/ValueInterpreter.java
@@ -1,3 +1,25 @@
+/* -*- c-basic-offset: 2; -*- */
+/*
+ * FreeDots -- MusicXML to braille music transcription
+ *
+ * Copyright 2008-2009 Mario Lang  All Rights Reserved.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details (a copy is included in the LICENSE.txt file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This software is maintained by Mario Lang <mlang@delysid.org>.
+ */
 package org.delysid.freedots.transcription;
 
 import java.util.ArrayList;
@@ -13,7 +35,7 @@ import org.delysid.freedots.model.TimeSignature;
 import org.delysid.freedots.musicxml.Note;
 
 /**
- * In braille music there are only 4 different types of note values.
+ * In braille music there are only four different types of note values.
  * The full range of whole to 128th notes is covered by reusing the same
  * types twice, a whole can also mean a 16th and so on.
  *
