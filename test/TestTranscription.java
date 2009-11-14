a0296f362f3d2c66c8a381807715b143902eba65
Mario Lang <mlang@delysid.org>
2009-11-14 00:47:21 +0000

    Test for one kind of value ambiguity resolution

--- test/TestTranscription.java
+++ test/TestTranscription.java
@@ -28,4 +28,25 @@ public class TestTranscription extends junit.framework.TestCase {
       + nl;
     assertEquals("pmia-1.xml", transcriber.toString(), expectedResult);
   }
+  public void testValueAmbiguity()
+    throws javax.xml.parsers.ParserConfigurationException,
+           java.io.IOException,
+           org.xml.sax.SAXException,
+           javax.xml.xpath.XPathExpressionException {
+    Options options = new Options(new String[] { "-w", "40" });
+    Score score = new Score("test/valueambiguity-1.xml");
+    Transcriber transcriber = new Transcriber(score, options);
+
+    final String nl = Transcriber.lineSeparator;
+    final String keyAndTimeSignature = "⠩⠼⠉⠲";
+    final String expectedResult = "         " + score.getComposer() + nl
+      + nl
+      + score.getParts().get(0).getName() + nl
+      + keyAndTimeSignature + nl
+      + "  "
+      + "⠐⠎⠉⠗⠉⠿⠗⠉⠟⠉⠯⠐⠢⠋⠃" + Braille.valueDistinction + "⠕"
+      + Braille.doubleBar + nl
+      + nl;
+    assertEquals("pmia-1.xml", transcriber.toString(), expectedResult);
+  }
 }
