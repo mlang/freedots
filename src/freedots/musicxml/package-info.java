bb0b2a0f50521fb809ec1516bb0d5fd92e89a9ab
Mario Lang <mlang@delysid.org>
2010-01-12 17:36:11 +0000

    We are in fact not really using the decorator pattern

--- src/freedots/musicxml/package-info.java
+++ src/freedots/musicxml/package-info.java
@@ -26,9 +26,9 @@
  * To load a MusicXML document, call one of the constructors of
  * {@link freedots.musicxml.Score}.
  * <p>
- * This package uses the
- * <a href="http://en.wikipedia.org/wiki/Decorator_pattern">decorator
- * pattern</a> to make it possible to extend the functionality of
+ * This package uses
+ * <a href="http://en.wikipedia.org/wiki/Wrapper_pattern">wrappers</a> to make
+ * it possible to extend the functionality of
  * {@link org.w3c.dom.Document} and {@link org.w3c.dom.Element}.
  */
 package freedots.musicxml;
