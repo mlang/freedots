4734e32f4176af8501bb8f227a739b717fc5afdd
Mario Lang <mlang@delysid.org>
2010-01-22 12:57:04 +0000

    [freedots.braille] Test BrailleTimeSignature

--- src/freedots/braille/Test.java
+++ src/freedots/braille/Test.java
@@ -1,13 +1,16 @@
 package freedots.braille;
 
+import freedots.music.TimeSignature;
+
 public class Test {
   public static final void main(String[] args) {
     BrailleList braille = new BrailleList();
+    braille.add(new BrailleTimeSignature(new TimeSignature(9, 16)));
+    braille.add(new NewLine());
     braille.add(new RightHandPart());
     braille.add(new BrailleChord());
     braille.add(new Space());
     braille.add(new OctaveSign(1));
-    braille.add(new NewLine());
     System.out.println(braille.toString() + " = " + braille.getDescription());
     for (int i = 0; i < braille.length(); i++) {
       final Atom sign = braille.getSignAtIndex(i);
@@ -17,5 +20,6 @@ public class Test {
       if (object != null)
         System.out.println("   i = "+braille.getIndexOfScoreObject(object));
     }
+
   }
 }
