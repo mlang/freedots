d59275939cc72624e7e813e07f4184b00064fd99
Mario Lang <mlang@delysid.org>
2010-01-22 01:42:29 +0000

    [freedots.braille] Test NewLine

--- src/freedots/braille/Test.java
+++ src/freedots/braille/Test.java
@@ -2,19 +2,20 @@ package freedots.braille;
 
 public class Test {
   public static final void main(String[] args) {
-    Compound line = new Compound();
-    line.add(new RightHandPart());
-    line.add(new BrailleNote(0, 3, 0, 1));
-    line.add(new Space());
-    line.add(new OctaveSign(1));
-    System.out.println(line.toString() + " = " + line.getDescription());
-    for (int i = 0; i < line.length(); i++) {
-      final Sign sign = line.getSignAtIndex(i);
-      final Object object = line.getScoreObjectAtIndex(i);
+    Compound braille = new Compound();
+    braille.add(new RightHandPart());
+    braille.add(new BrailleNote(0, 3, 0, 1));
+    braille.add(new Space());
+    braille.add(new OctaveSign(1));
+    braille.add(new NewLine());
+    System.out.println(braille.toString() + " = " + braille.getDescription());
+    for (int i = 0; i < braille.length(); i++) {
+      final Sign sign = braille.getSignAtIndex(i);
+      final Object object = braille.getScoreObjectAtIndex(i);
       System.out.println("i = "+i+": '"+sign+"' = "+sign.getDescription());
       System.out.println("  "+object);
       if (object != null)
-        System.out.println("   i = "+line.getIndexOfScoreObject(object));
+        System.out.println("   i = "+braille.getIndexOfScoreObject(object));
     }
   }
 }
