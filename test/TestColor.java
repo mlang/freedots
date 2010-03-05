1aded0699a5ce8aa7e3925da6f6172c38aa516f6
Amélie Marzin <marzin@enseirb-matmeca.fr>
2010-03-05 15:55:31 +0000

    Changing double bar sign color in TestColor

--- test/TestColor.java
+++ test/TestColor.java
@@ -12,7 +12,7 @@ public class TestColor extends junit.framework.TestCase {
     SignColorMap scm = SignColorMap.DEFAULT;
     assertEquals("accent color", new Color(0, 250, 154),
                                  scm.get(BrailleNote.AccentSign.class));
-    assertEquals("double bar color", Color.red,
+    assertEquals("double bar color", new Color(255, 126, 0),
                                      scm.get(DoubleBarSign.class));
   }
 }
