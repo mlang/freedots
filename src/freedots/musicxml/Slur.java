3722f975f4dc987c978cb894bd13904e6d5f3604
Alex Repain <repain@enseirb-matmeca.fr>
2010-04-10 16:11:59 +0000

    Fixed a bug at TranscriptionTest runtime, see diff for freedots.musicxml.Note

--- src/freedots/musicxml/Slur.java
+++ src/freedots/musicxml/Slur.java
@@ -30,7 +30,7 @@ package freedots.musicxml;
  * of the slur.
  */
 public class Slur extends freedots.music.Slur<Note> {
-  Slur(final Note initialNote) { super(initialNote); }
+  Slur(final Note initialNote) { super((Note)initialNote); }
   @Override
   public final boolean add(final Note note) {
     if (super.add(note)) {
